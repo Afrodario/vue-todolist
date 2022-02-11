4a6ba5d12bbd339f790ceb80c1b0e7954eb72473
Dario La Tegola <dariolategola@gmail.com>
2022-02-11 15:39:31 +0100

    Prototipo Milestone 1

---
+++ main.js
@@ -0,0 +1,26 @@
+const app = new Vue(
+    {
+        el: "#root",
+        data: {
+            newAlbum: "",
+            albums: [
+                {
+                  text: "Rhythm Nation 1814",
+                  done: true  
+                },
+                {
+                    text: "Ray of Light",
+                    done: true  
+                  },
+                {
+                    text: "Sign o' the Times",
+                    done: true  
+                }
+            ]
+        },
+
+        methods: {
+
+        }
+    }
+);
